96d949dda7b7c5003f734f7849371aa18328e230
zhouyepang <pangtian2006@gmail.com>
2018-09-24 15:02:47 +1000

    test signup

---
+++ app/src/main/java/com/example/zhouyepang/instagramapp/FeedActivity.java
@@ -0,0 +1,24 @@
+package com.example.zhouyepang.instagramapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+public class FeedActivity extends AppCompatActivity {
+
+    FirebaseUser fbUser;
+    DatabaseReference database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+
+    /*public void uploadImage(View view) {
+
+    }*/
+}
