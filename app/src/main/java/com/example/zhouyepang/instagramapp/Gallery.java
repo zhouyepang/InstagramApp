01b2663fa27734ef8bea2cbb6ef8e81e283e57f5
RoryKirsi <qq441072110@gmail.com>
2018-10-02 16:04:02 +1000

    Done upload image from gallery

--- app/src/main/java/com/example/zhouyepang/instagramapp/Gallery.java
+++ app/src/main/java/com/example/zhouyepang/instagramapp/Gallery.java
@@ -1,18 +1,97 @@
 package com.example.zhouyepang.instagramapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 public class Gallery extends Fragment {
+    private Button btnSelect, btnNext;
+    private ImageView photoPreview;
+    private static final int CROP_PHOTO = 2;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 6;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE2 = 7;
+    static final int RC_PERMISSION_READ_EXTERNAL_STORAGE = 1;
+    static final int RC_IMAGE_GALLERY = 2;
+    Uri imageUri;
+    Context applicationContext;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.gallery, container, false);
+        View v = inflater.inflate(R.layout.gallery, container, false);
+
+        //bind image view
+        photoPreview = (ImageView) v.findViewById(R.id.photoPreview);
+        //bind button
+        btnSelect = (Button) v.findViewById(R.id.btn_click_select);
+        btnNext = (Button) v.findViewById(R.id.btn_click_next);
+        applicationContext = UploadSelect.getContextOfApplication();
+        btnSelect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openGallery(view);
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (imageUri != null) {
+                    Intent uploadOptionsIntent = new Intent(applicationContext, UploadOptions.class);
+                    uploadOptionsIntent.putExtra("imageUri", imageUri.toString());
+                    startActivity(uploadOptionsIntent);
+                }
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == RC_PERMISSION_READ_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                choosePhoto();
+            }
+        }
+    }
+
+    public void openGallery(View view) {
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, RC_PERMISSION_READ_EXTERNAL_STORAGE);
+        } else {
+            choosePhoto();
+        }
+    }
+
+    private void choosePhoto(){
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, RC_IMAGE_GALLERY);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_IMAGE_GALLERY && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+            photoPreview.setImageURI(imageUri);
+        }
     }
 }
--- app/src/main/java/com/example/zhouyepang/instagramapp/UploadSelect.java
+++ app/src/main/java/com/example/zhouyepang/instagramapp/UploadSelect.java
@@ -1,33 +1,15 @@
 package com.example.zhouyepang.instagramapp;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
+import android.content.Context;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import java.io.File;
 
 public class UploadSelect extends AppCompatActivity  {
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
     private UploadPhotoPagerAdapter uploadPhotoPagerAdapter;
-    private Button btnSelect, btnNext;
-    private ImageView photoPreview;
-    private static final int CROP_PHOTO = 2;
-    private static final int REQUEST_CODE_PICK_IMAGE = 100;
-    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 6;
-    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE2 = 7;
-    private File output;
-    private Uri imageUri;
+
     private TabLayout.Tab gallery;
     private TabLayout.Tab camera;
 
@@ -38,8 +20,13 @@ public class UploadSelect extends AppCompatActivity  {
         setContentView(R.layout.upload_select);
 
         initViews();
+        contextOfApplication = getApplicationContext();
     }
-
+    public static Context contextOfApplication;
+    public static Context getContextOfApplication()
+    {
+        return contextOfApplication;
+    }
     private void initViews() {
 
         //bind ViewPagerAdapter with Fragment
@@ -55,51 +42,10 @@ public class UploadSelect extends AppCompatActivity  {
         gallery = mTabLayout.getTabAt(0);
         camera = mTabLayout.getTabAt(1);
 
-        //initial image view
-        photoPreview = (ImageView) findViewById(R.id.photoPreview);
-        //bind button
-        btnSelect = (Button) findViewById(R.id.btn_click_select);
-        btnNext = (Button) findViewById(R.id.btn_click_next);
+
     }
 
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_click_select:
-                choosePhoto();
-                break;
-            case R.id.btn_click_next:
 
-                break;
-            default:
-                break;
-        }
-    }
 
-    void choosePhoto(){
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(gallery, REQUEST_CODE_PICK_IMAGE);
-    }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case REQUEST_CODE_PICK_IMAGE:
-                if (resultCode == RESULT_OK) {
-                    try {
-                        Uri uri = data.getData();
-                        Bitmap bit = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
-                        photoPreview.setImageBitmap(bit);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        Log.d("tag",e.getMessage());
-                        Toast.makeText(this,"Crash",Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else{
-                    Log.i("Fail:", "Fail");
-                }
-                break;
-            default:
-                break;
-        }
-    }
 }
